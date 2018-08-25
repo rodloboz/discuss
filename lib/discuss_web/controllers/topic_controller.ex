67818e643f94908ba93dd4cf210380e3192e3562
Rui Freitas <dinnerpartysaboteur@gmail.com>
2018-08-25 09:39:04 +0100

    topics model and topic#new

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -1,7 +1,22 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
+  alias Discuss.Topic
+
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = Topic.changeset(%Topic{}, %{})
+    render conn, "new.html", changeset: changeset
   end
+
+  # defp render_form(conn, action, announcement) do
+  #   changeset = Announcement.create_changeset(announcement, %{})
+  #   interests = Repo.all(Interest)
+  #   users = Repo.all(User.active)
+
+  #   render(conn, action, %{
+  #     changeset: changeset,
+  #     interests: interests,
+  #     users: users,
+  #   })
+  # end
 end
--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -17,7 +17,7 @@ defmodule DiscussWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    resources "/topics", TopicController, only: [:new]
+    resources "/topics", TopicController, only: [:new, :create]
   end
 
   # Other scopes may use custom stacks.
